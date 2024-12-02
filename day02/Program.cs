4ada3633b2a2576fc3226f0d8c0dcbf3ce3774b5
Jean Gentile <jean@thegentiles.com>
2024-12-02 13:32:18 -0500

    Starting Day02

---
+++ day02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace day02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+    }
+}
