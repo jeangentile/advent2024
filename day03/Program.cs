965b088752fe74e8588b5bc21c7690003b04faa2
Jean Gentile <jean@thegentiles.com>
2024-12-03 09:06:38 -0500

    Day03 starts

---
+++ day03/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace day03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Day 03!");
+ 
+        var reader = System.IO.File.ReadAllText("c:\\dev\\advent2024\\advent2024\\day03\\input.txt");
+
+
+    }
+}
