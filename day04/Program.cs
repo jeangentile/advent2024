f1fc3fbf7fd4795b98100e47a371c9060ee6cde7
Jean Gentile <jean@thegentiles.com>
2024-12-07 11:31:03 -0500

    Day 5 give up

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using Microsoft.VisualBasic;
 
 namespace day04;
@@ -22,10 +23,72 @@ class Program
         Console.WriteLine($"Grid Width: {gridWidth}");
         Console.WriteLine($"Grid Length: {gridLength}");
 
-        ScanGrid();
+        Part2();
     }
 
-    static void ScanGrid()
+
+    static void Part2()
+    {
+        var matchCounter = 0;
+        for(var row = 0; row < gridLength - 2; row++)
+        {
+            for(var col = 0; col < gridWidth - 2; col++)
+            {
+                var stringPart1 = gridLines[row].Substring(col,3);
+                var stringPart2 = gridLines[row+1].Substring(col,3);
+                var stringPart3 = gridLines[row+2].Substring(col,3);
+                if(Regex.IsMatch(stringPart1, "M.S"))
+                {
+                    if(Regex.IsMatch(stringPart2, ".A."))
+                    {
+                        if(Regex.IsMatch(stringPart3, "M.S"))
+                        {
+                            Console.WriteLine($"MATCH! {stringPart1}, {stringPart2}, {stringPart3}");
+                            matchCounter++;
+                        }
+                    }
+                }
+                if(Regex.IsMatch(stringPart1, "S.M"))
+                {
+                    if(Regex.IsMatch(stringPart2, ".A."))
+                    {
+                        if(Regex.IsMatch(stringPart3, "S.M"))
+                        {
+                            Console.WriteLine($"MATCH! {stringPart1}, {stringPart2}, {stringPart3}");
+                            matchCounter++;
+                        }
+                    }
+                }
+                if(Regex.IsMatch(stringPart1, "S.S"))
+                {
+                    if(Regex.IsMatch(stringPart2, ".A."))
+                    {
+                        if(Regex.IsMatch(stringPart3, "M.M"))
+                        {
+                            Console.WriteLine($"MATCH! {stringPart1}, {stringPart2}, {stringPart3}");
+                            matchCounter++;
+                        }
+                    }
+                }
+                if(Regex.IsMatch(stringPart1, "M.M"))
+                {
+                    if(Regex.IsMatch(stringPart2, ".A."))
+                    {
+                        if(Regex.IsMatch(stringPart3, "S.S"))
+                        {
+                            Console.WriteLine($"MATCH! {stringPart1}, {stringPart2}, {stringPart3}");
+                            matchCounter++;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        Console.WriteLine($"Matches {matchCounter}");
+    }
+
+    static void Part1()
     {
         //lines
         for(var yPos = 0; yPos < gridLength; yPos++)
@@ -34,6 +97,8 @@ class Program
             Console.WriteLine($"Line [{yPos}]: {line}");
             allLines.Add(line);
         }
+        Console.WriteLine();
+        Console.WriteLine();
 
         //columns
         for(var yPos = 0; yPos < gridLength; yPos++)
@@ -46,49 +111,85 @@ class Program
             Console.WriteLine($"Column [{yPos}]: {column}");
             allLines.Add(column);
         }
+        Console.WriteLine();
+        Console.WriteLine();
 
         //diagonals down
-        for (var lineOffset = 0; lineOffset < gridLength; lineOffset++)
+        for (var startingRowDown = 0; startingRowDown < gridLength; startingRowDown++)
         {
-            Console.WriteLine($"Diagonal starting row DOWN: {lineOffset}");
+            Console.WriteLine($"Diagonal starting row DOWN: {startingRowDown}");
 
-            for(var lineCounter = lineOffset; lineCounter < gridLength; lineCounter++)
+            var y = startingRowDown;
+            var diagonal = "";
+            for(var x = 0; x < gridWidth - startingRowDown; x++)
             {
-                var y = lineCounter;
-                var diagonal = "";
-                for(var x = 0; x < gridWidth - lineCounter; x++)
-                {
-                    diagonal += gridLines[x].Substring(y,1);
-
-                    Console.Write($"({x},{y})");
-                    //Console.Write($"{gridLines[x].Substring(y,1)} ");
-                    y++;
-                }
-
-                Console.WriteLine($"Diagonal DOWN [{lineCounter}]: {diagonal}");
-                allLines.Add(diagonal);
+                diagonal += gridLines[y].Substring(x,1);
+                Console.Write($"(row{y},col{x})");
+                y++;
             }
+            Console.WriteLine($"Diagonal DOWN row [{y}]: {diagonal}");
+            allLines.Add(diagonal);
         }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //diagonals across
+        for (var startingColAcross = 1; startingColAcross < gridWidth; startingColAcross++)
+        {
+            Console.WriteLine($"Diagonal starting columns ACROSS: {startingColAcross}");
+
+            var y = 0;
+            var diagonal = "";
+            for(var x = startingColAcross; x < gridWidth; x++)
+            {
+                diagonal += gridLines[y].Substring(x,1);
+                Console.Write($"(row{y},col{x})");
+                y++;
+            }
+            Console.WriteLine($"Diagonal ACROSS column [{startingColAcross}]: {diagonal}");
+            allLines.Add(diagonal);
+        }
+        Console.WriteLine();
+        Console.WriteLine();
 
         //diagonals up
-        for (var startingRow = gridLength - 1; startingRow > 0; startingRow--)
+        for (var startingRowUp = gridLength - 1; startingRowUp > -1; startingRowUp--)
         {
-            Console.WriteLine($"Diagonal starting row UP: {startingRow}");
+            Console.WriteLine($"Diagonal starting row UP: {startingRowUp}");
 
-            for(var lineRepeat = 0; lineRepeat < startingRow + 1; lineRepeat++)
+            var y = startingRowUp;
+            var diagonal = "";
+            for(var x = 0; x < gridWidth - (gridLength - 1 - startingRowUp); x++)
             {
-                var y = startingRow;
-                var diagonal = "";
-                for(var x = lineRepeat; x < gridWidth - (gridLength - startingRow - 1); x++)
-                {
-                        Console.Write($"({x},{y})");
-                        diagonal += gridLines[y].Substring(x,1);
-                        y--;
-                }
-                Console.WriteLine($"Diagonal UP [{startingRow}]: {diagonal}");
-                allLines.Add(diagonal);
+                diagonal += gridLines[y].Substring(x,1);
+                Console.Write($"(row{y},col{x})");
+                y--;
             }
+            Console.WriteLine($"Diagonal UP row [{y}]: {diagonal}");
+            allLines.Add(diagonal);
         }
+        Console.WriteLine();
+        Console.WriteLine();
+        
+        //diagonals upacross
+        for (var startingColUpAcross = 1; startingColUpAcross < gridWidth; startingColUpAcross++)
+        {
+            Console.WriteLine($"Diagonal starting columns UPACROSS: {startingColUpAcross}");
+
+            var y = gridLength - 1;
+            var diagonal = "";
+            for(var x = startingColUpAcross; x < gridWidth; x++)
+            {
+                diagonal += gridLines[y].Substring(x,1);
+                Console.Write($"(row{y},col{x})");
+                y--;
+            }
+            Console.WriteLine($"Diagonal UPACROSS column [{startingColUpAcross}]: {diagonal}");
+            allLines.Add(diagonal);
+        }
+        Console.WriteLine();
+        Console.WriteLine();       
+
 
         var totalMatches = 0;
         totalMatches += FindMatches("XMAS");
---
+++ day05/Program.cs
@@ -0,0 +1,142 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using Microsoft.VisualBasic;
+
+
+Console.WriteLine("Hello, World!");
+
+var rulestxt = File.ReadAllText("c:\\dev\\advent2024\\advent2024\\day05\\rules.txt");
+var input = File.ReadAllText("c:\\dev\\advent2024\\advent2024\\day05\\input.txt");
+
+var rules = new Dictionary<int, RuleMap>();
+foreach(var ruleLine in rulestxt.Split(Environment.NewLine))
+{
+    var ruleLineSplit = ruleLine.Split("|");
+
+    var pageLeft = int.Parse(ruleLineSplit[0]);
+    var pageRight = int.Parse(ruleLineSplit[1]);
+
+    if (!rules.TryGetValue(pageLeft, out var ruleMapLeft))
+    {
+        ruleMapLeft = new RuleMap(pageLeft);
+        rules.Add(pageLeft, ruleMapLeft);
+    }
+    ruleMapLeft.PagesAfter.Add(pageRight);
+    rules[pageLeft] = ruleMapLeft;
+
+    if (!rules.TryGetValue(pageRight, out var ruleMapRight))
+    {
+        ruleMapRight = new RuleMap(pageRight);
+        rules.Add(pageRight, ruleMapRight);
+    }
+    ruleMapRight.PagesBefore.Add(pageLeft);
+    rules[pageRight] = ruleMapRight;
+
+}
+
+var middlePages = 0;
+
+foreach(var inputLine in input.Split(Environment.NewLine))
+{
+    var lineIsValid = EvaluateLine(inputLine);
+    var inputLineSplit = inputLine.Split(",");
+
+    if (lineIsValid)
+    {
+        var middlePage = int.Parse(inputLineSplit[inputLineSplit.Length /2]);
+        middlePages += middlePage;
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"##Fixing broken row {inputLine}");
+        var newLine = new List<int>();
+
+        for(var splitPosition = 0; splitPosition < inputLineSplit.Length; splitPosition++)
+        {
+            var page = int.Parse(inputLineSplit[splitPosition]);
+            Console.WriteLine($"  >Page: {page} ");
+            if (!rules.TryGetValue(page, out var pageRules))
+            {
+                newLine.Add(page);
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+}
+
+bool EvaluateLine(string inputLine)
+{
+    Console.WriteLine($"Evaluating {inputLine}");
+    
+    var inputLineSplit = inputLine.Split(",");
+
+
+    for(var splitPosition = 0; splitPosition < inputLineSplit.Length; splitPosition++)
+    {
+        var page = int.Parse(inputLineSplit[splitPosition]);
+        Console.WriteLine($"  >Page: {page} ");
+        
+        //find matching rules for this page        
+        if (rules.TryGetValue(page, out var pageRules))
+        {
+            //check all pages after this page
+            for(var positionAfter = splitPosition + 1; positionAfter < inputLineSplit.Length; positionAfter++)
+            {
+                var pageAfter = int.Parse(inputLineSplit[positionAfter]);
+                Console.Write($"         Page After {pageAfter} ");
+
+                var canBeAfter = pageRules.PagesAfter.Contains(pageAfter);
+                Console.Write($"{canBeAfter} ");
+
+                if (!canBeAfter)
+                {
+                    return false;
+                }
+
+                Console.WriteLine();
+            }
+
+            //check all pages before this page
+            for(var positionBefore = splitPosition - 1; positionBefore > -1; positionBefore--)
+            {
+                var pageBefore = int.Parse(inputLineSplit[positionBefore]);
+                Console.Write($"         Page Before {pageBefore} ");
+
+                var canBeBefore = pageRules.PagesBefore.Contains(pageBefore);
+                Console.Write($"{canBeBefore} ");
+
+                if (!canBeBefore)
+                {
+                    return false;
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+    Console.WriteLine();
+
+    return true;
+}
+
+
+Console.WriteLine($"Valid Middle Pages: {middlePages}");
+
+public class RuleMap
+{
+    public int Page;
+    public List<int> PagesBefore = new List<int>();
+    public List<int> PagesAfter = new List<int>();
+
+    public RuleMap(int page)
+    {
+        Page = page;
+    }
+}
