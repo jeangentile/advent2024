a8d9d52c819c7c8787eb9235ca2499108113990e
Jean Gentile <jean@thegentiles.com>
2024-12-07 11:34:20 -0500

    Day06 scaffolded

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllText("c:\\dev\\advent2024\\advent2024\\day06\\input-test.txt");
+
+
+
