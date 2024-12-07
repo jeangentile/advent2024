19dcbc299b753a1952611a1047f00d88904a3364
Jean Gentile <jean@thegentiles.com>
2024-12-07 12:10:30 -0500

    input files

--- day06/Program.cs
+++ day06/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Hello, World!");
 
 
 var j = new Day6Part1();
-var result = await j.SolveAsync(new StreamReader("c:\\dev\\advent2024\\advent2024\\day06\\input.txt"));
+var result = await j.SolveAsync(new StreamReader("c:\\dev\\advent2024\\advent2024\\day06\\input-test.txt"));
 Console.WriteLine(result);
 
 public partial class Day6Part1
